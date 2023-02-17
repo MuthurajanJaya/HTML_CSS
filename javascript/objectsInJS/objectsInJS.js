68728a10bcab2e1ae104cb2fafabe00ac1e036ed
JAYABHARATHI <jaya.muthurajan@aspiresystems.com>
2023-02-17 18:00:51 +0530

    Functions Objects and Type Conversion

---
+++ javascript/objectsInJS/objectsInJS.js
@@ -0,0 +1,7 @@
+let initialObject = {
+    name: "JAI",
+    Company: "Aspire System",
+    Team: "LAMP"
+}
+console.log(initialObject.name, typeof initialObject)
+console.log(initialObject["name"])
